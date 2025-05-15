ee7d8195fb0e65ffb3f60f943c0313aaac89ee3a
Salhi Anis <87819764+Salhianis1@users.noreply.github.com>
2025-05-15 09:28:28 +0100

    Update vite.config.js

--- mern/frontend/vite.config.js
+++ mern/frontend/vite.config.js
@@ -6,6 +6,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['app.local']
+    allowedHosts: ['app.local', 'localhost', 'all']
   }
 })
